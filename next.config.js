e139316d3971df225a9ad1b163f8eec3ba9a6e80
Cristopher Areche <cristopherareche764@gmail.com>
2023-12-30 15:24:27 -0400

    env hidden

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    SUPABASE_URL: "https://kjiealevqywkdotnqxmo.supabase.co",
+    SUPABASE_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqaWVhbGV2cXl3a2RvdG5xeG1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM4ODU1MjQsImV4cCI6MjAxOTQ2MTUyNH0.1Mmy16xQel-PS3oCvow0f-h0be9gwst2pvwssz7-vbM",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kjiealevqywkdotnqxmo.supabase.co";
-const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqaWVhbGV2cXl3a2RvdG5xeG1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM4ODU1MjQsImV4cCI6MjAxOTQ2MTUyNH0.1Mmy16xQel-PS3oCvow0f-h0be9gwst2pvwssz7-vbM`;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
